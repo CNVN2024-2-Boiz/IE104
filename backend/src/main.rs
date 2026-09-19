8a3272a0770ca88966e31e61b274080bbd1fe433
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-19 23:02:29 +0700

    fix: test rust runtime

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,19 @@
+use actix_web::{App, HttpServer, Result, get, web};
+
+#[get("/{id}")]
+async fn greet(path: web::Path<String>) -> Result<String> {
+    let id = path.into_inner();
+    Ok(format!("The id is: {}", id))
+}
+
 #[actix_web::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
 
